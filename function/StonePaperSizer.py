a62e697754b4cd8d097a4af7440e84e4dcf39666
Vaibhav Karkute <101312171+Vaibhavkarkute@users.noreply.github.com>
2023-02-12 22:42:57 +0530

    Update StonePaperSizer.py
    Second Commit

--- function/StonePaperSizer.py
+++ function/StonePaperSizer.py
@@ -5,37 +5,37 @@ def gameWin(comp,you):
     #if the two value are same , declare as tie !
     if comp==you:
         return None
-    # Check for all possibilities when computer chose st
-    elif comp =='stone':
+    # Check for all possibilities when computer chose r
+    elif comp =='rock':
         if you=='paper':
             return True
-        elif you =='sizer':
+        elif you =='scissors':
             return False
     # Check for all possibilities when computer chose p
     elif comp=='paper':
-        if you=='sizer':
+        if you=='scissors':
             return True
-        elif you =='stone':
+        elif you =='rock':
             return False
     # Check for all possibilities when computer chose si
-    elif comp=='sizer':
+    elif comp=='scissors':
         if you =='paper':
             return True
-        elif you =='stone':
+        elif you =='rock':
             return False
 
 
 
-print ("Comp Turn : Stone (st) Paper (p) And Sizer (si) :")
+print ("Comp Turn : rock (r) Paper (p) And scissors (si) :")
 randNo = random.randint(1,3)
 if randNo==1:
-    comp = 'stone'
+    comp = 'rock'
 elif randNo==2:
     comp = 'paper'   
 elif randNo==3:
-    comp = 'sizersto'    
+    comp = 'scissors'    
 
-you = input("You Turn : Stone(st) Paper(p) And Sizer(si) :")
+you = input("You Turn : rock(r) Paper(p) And scissors(si) :")
 a=gameWin(comp,you)
 
 print (f"Comp chose {comp}")
